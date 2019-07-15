b4d1c9ed57a2722dff0b16b6c5b0d4dd1c03ddd1
Joseph Harrison-Lim <Josephharrisonlim@gmail.com>
2019-07-14 22:16:42 -0400

    Fix dynamic module loading

--- packages/core/src/application.ts
+++ packages/core/src/application.ts
@@ -1,49 +1,54 @@
 import readline from 'readline';
-import repl from 'repl';
+import repl, { REPLServer } from 'repl';
 import path from 'path';
 import { Transform, Readable } from 'stream';
-import { readdir, stat } from 'fs';
+import fs from 'fs';
 import { promisify } from 'util';
 
 export class Application {
-    private readonly _repl: repl.REPLServer;
+    private _repl?: repl.REPLServer;
+    private readonly _replStream: Readable;
     constructor() {
         // Use a separate stream for the repl since we want to filter out ctrl keys before writing
-        const replStream = new Readable();
-        replStream.on('error', replStream.destroy);
-        process.stdin.on('data', data => {
-            // Todo: Check for ctrl keys
-            // Todo!: Data isn't currently being emitted back to stdout
-            replStream.push(data);  // forward data to replStream
-        });
+        // TODO: Implement _read()
+        this._replStream = new Readable();
         this._repl = repl.start({
-            input: replStream,
+            input: this._replStream,
             output: process.stdout,
             prompt: 'prowler: '
         });
-        replStream.resume();
     }
 
     start() {
+        this._replStream.on('error', this._replStream.destroy);
+        process.stdin.on('data', data => {
+            // Todo: Check for ctrl keys
+            // Todo!: Data isn't currently being emitted back to stdout
+            this._replStream.push(data);  // forward data to replStream
+        });
+        this._replStream.resume();
         process.stdin.resume();
     }
 
     async loadBuiltins() {
-        const rd = promisify(readdir);
-        const s = promisify(stat);
-        const directoryContents = await rd(path.join(__dirname, 'builtins'));
-        for (const filePath of directoryContents) {
-            const fileStat = await s(filePath);
-            const { name } = path.parse(filePath);
+        const readDir = promisify(fs.readdir);
+        const stat = promisify(fs.stat);
+        const builtinsPath = path.join(__dirname, 'builtins');
+        const directoryContents = await readDir(builtinsPath);
+
+        for (const fileName of directoryContents) {
+            const filePath = path.join(builtinsPath, fileName);
+            const fileStat = await stat(filePath);
 
             if (fileStat.isDirectory())
                 continue;
 
-            if(name.match(/\.md|\.spec|\.test/) !== null || !name.match(/\.js/))
+            if(fileName.match(/\.md|\.spec|\.test/) !== null || !fileName.match(/\.js/))
                 continue;
 
-            const mod = await import(name);
-            this._repl.defineCommand(name.replace('.js', ''), mod);
+            console.log('Importing ' + fileName);
+            const mod = await import(filePath);
+            this._repl!.defineCommand(fileName.replace('.js', ''), mod);
         }
     }
 }
