82a6aff43759da99ecc2fadf6dffeb24b87cf2b5
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2019-07-26 22:43:36 -0400

    Add colour ls

--- packages/core/src/profile/colour.ts
+++ packages/core/src/profile/colour.ts
@@ -1,8 +1,10 @@
 import chalk from 'chalk';
-export class ColourProfile {
 
+// TODO: Refactor to its own module
+export class ColourProfile {
     constructor(
-        public primary = chalk.gray
+        public primary = chalk.gray,
+        public secondary = chalk.green
     ) {
         
     }
--- packages/ls/src/ls.ts
+++ packages/ls/src/ls.ts
@@ -1,11 +1,21 @@
 import chalk from 'chalk';
 import { REPLCommand } from 'repl';
-import { Command } from 'commander';
-
-
+import { readdirSync, statSync } from 'fs';
 
 export function ls(args: string) {
+    const files = readdirSync(process.cwd());
+    let output = '';
+    const width = files.reduce((prev, cur) => prev > cur.length ? prev : cur.length, 0);
+    
+    for (let i = 0; i < files.length; i++) {
+        if (i > 0 && i % 4 === 0) output += '\n';
+        const padding = ' '.repeat(width - files[i].length);
 
+        // TODO: Use profile colours
+        const colour = statSync(files[i]).isFile() ? chalk.gray : chalk.green;
+        output += `${colour(files[i])}${padding}\t`;
+    }
+    return output;
 }
 
 const cmd: REPLCommand = {
